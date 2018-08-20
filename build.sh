433bde4e0a641c3197bd7bfe0171dd9995cb2223
Jan-Erik Rediger <janerik@fnordig.de>
2018-08-20 21:42:12 +0200

    Install and update relevant toolchains

--- build.sh
+++ build.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 stable() {
+  rustup toolchain add stable >&2
   rustup run stable rustc --version | egrep -o '1\.[0-9]+\.[0-9]+'
 }
 
 beta() {
+  rustup toolchain add beta >&2
   rustup run beta rustc --version | egrep -o '1\.[0-9]+\.[0-9]+'
 }
 
 nightly() {
+  rustup toolchain add nightly >&2
   rustup run nightly rustc --version | \
     sed 's/.\+\(1\.[0-9]\+\.[0-9]\+\)-nightly ([0-9a-f]\+ \(.\+\))/\1 (\2)/'
 }
 
+rustup update
 
 s=$(stable)
 b=$(beta)
 n=$(nightly)
+
 sed \
   -e "s/{STABLE}/$s/" \
   -e "s/{BETA}/$b/" \
