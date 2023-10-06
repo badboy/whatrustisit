6939aaaeebc867222ee0a2514c525b0a605aa785
Riven Skaye <riven@tae.moe>
2023-10-06 13:38:41 +0200

    Probe static.rust-lang.org for version info

--- build.sh
+++ build.sh
@@ -6,33 +6,36 @@ else
   SED=sed
 fi
 
-stable() {
-  rustup toolchain add stable >&2
-  rustup run stable rustc --version | egrep -o '1\.[0-9]+\.[0-9]+'
+rustc_crawl() {
+  ptrn='\[pkg\.rust\]\nversion = "\K[^"]+'
+  relday=$(date -I)  # Today's date
+  # Rust stable updates happen every 6 weeks, starting at 2015-12-10.
+  # This would be a very clean method of extracting versions, were it not for patches.
+  # Rust 1.72.1 was released 21 days after 1.72.0 for example, but 1.73.0 was still
+  # on schedule. This makes last modified time of the stable file unreliable.
+  if [[ "$1" = 'stable' ]] ; then
+      rustup run stable rustc --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+      return 0
+  fi
+
+  # nightly releases should be found on the first check here, making it equivalent to an if,
+  # but beta releases are a pain in the behind to track down, so we probe a few days for those.
+  # On the off chance this ever runs before nightly builds, nightly falls back to yesterday's.
+  while [[ $(curl -s "https://static.rust-lang.org/?prefix=dist/$relday" | grep -c "$1") == '0' ]] ; do
+    relday=$(date -I -d "$relday -1 day")
+  done
+  # Extract whatever version information upstream provides!
+  curl -s "https://static.rust-lang.org/dist/$relday/channel-rust-$1.toml" | grep -ozP "$ptrn" | tr -d '\0'
 }
 
-beta() {
-  rustup toolchain add beta >&2
-  rustup run beta rustc --version | \
-    $SED 's/.\+\(1\.[0-9]\+\.[0-9]\+[^ ]*\).*/\1/'
-}
+rustup update stable >&2
 
-nightly() {
-  rustup toolchain add nightly >&2
-  rustup run nightly rustc --version | \
-    $SED 's/.\+\(1\.[0-9]\+\.[0-9]\+\)-nightly ([0-9a-f]\+ \(.\+\))/\1 (\2)/'
-}
+s=$(rustc_crawl 'stable')
+b=$(rustc_crawl 'beta' | $SED 's/\(.*beta.1\s(\).*\s\(.*)\)/\1\2/')
+n=$(rustc_crawl 'nightly' | $SED 's/\(.*\)-nightly\(\s(\).*\s\(.*)\)/\1\2\3/')
 
-pickdate() {
-  echo "$1" | $SED 's/\(1\.[0-9]\+\.[0-9]\+\) (\(.\+\))/\2/'
-}
-
-rustup update
-
-s=$(stable)
-b=$(beta)
-n=$(nightly)
-nightlyDate=$(pickdate "$n")
+betaDate=$($SED 's/.*(\(.*\))/\1/' <<< $b)
+nightlyDate=$($SED 's/.*(\(.*\))/\1/' <<< $n)
 
 $SED \
   -e "s/{STABLE}/$s/" \
@@ -50,7 +53,7 @@ EOS
 # Maybe we can eventually parse https://static.rust-lang.org/manifests.txt
 cat <<EOS > beta
 [toolchain]
-channel = "beta"
+channel = "beta-${betaDate}"
 EOS
 
 cat <<EOS > nightly
