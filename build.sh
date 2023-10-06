38af289ba2cf7c36d58a11a094756758b43d5e59
Riven Skaye <riven@tae.moe>
2023-10-06 15:57:48 +0200

    Add GNU grep, update toot version files

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@ else
   SED=sed
 fi
 
+if command -v ggrep 2>/dev/null; then
+  GREP=ggrep
+else
+  GREP=grep
+fi
+
 rustc_crawl() {
   ptrn='\[pkg\.rust\]\nversion = "\K[^"]+'
   relday=$(date -I)  # Today's date
@@ -14,27 +20,29 @@ rustc_crawl() {
   # Rust 1.72.1 was released 21 days after 1.72.0 for example, but 1.73.0 was still
   # on schedule. This makes last modified time of the stable file unreliable.
   if [[ "$1" = 'stable' ]] ; then
-      rustup run stable rustc --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+'
+      rustup run stable rustc --version > ./toot/stable
+      $GREP -Eo '[0-9]+\.[0-9]+\.[0-9]+' ./toot/stable
       return 0
   fi
 
   # nightly releases should be found on the first check here, making it equivalent to an if,
   # but beta releases are a pain in the behind to track down, so we probe a few days for those.
   # On the off chance this ever runs before nightly builds, nightly falls back to yesterday's.
-  while [[ $(curl -s "https://static.rust-lang.org/?prefix=dist/$relday" | grep -c "$1") == '0' ]] ; do
+  while [[ $(curl -s "https://static.rust-lang.org/?prefix=dist/$relday" | $GREP -c "$1") == '0' ]] ; do
     relday=$(date -I -d "$relday -1 day")
   done
   # Extract whatever version information upstream provides!
-  curl -s "https://static.rust-lang.org/dist/$relday/channel-rust-$1.toml" | grep -ozP "$ptrn" | tr -d '\0'
+  curl -s "https://static.rust-lang.org/dist/$relday/channel-rust-$1.toml" | $GREP -ozP "$ptrn" | tr -d '\0'
 }
 
 rustup update stable >&2
 
 s=$(rustc_crawl 'stable')
-b=$(rustc_crawl 'beta' | $SED 's/\(.*beta.*\s(\).*\s\(.*)\)/\1\2/')
+b=$(rustc_crawl 'beta')
+echo "rustc $b" > ./toot/beta
+b=$($SED 's/\(.*beta.*\s(\).*\s\(.*)\)/\1\2/' <<< $b)
 n=$(rustc_crawl 'nightly' | $SED 's/\(.*\)-nightly\(\s(\).*\s\(.*)\)/\1\2\3/')
 
-betaDate=$($SED 's/.*(\(.*\))/\1/' <<< $b)
 nightlyDate=$($SED 's/.*(\(.*\))/\1/' <<< $n)
 
 $SED \
